49e17bb126c20a74f9667ccde75373a7ce9e664e
Alexander Neumann <alexander@bumpern.de>
2016-07-09 16:49:49 +0200

    Highlight local and global fields in different colors

--- src/cmds/erpel/cmd_show.go
+++ src/cmds/erpel/cmd_show.go
@@ -3,9 +3,7 @@ package main
 import (
 	"erpel"
 	"errors"
-	"io"
-	"os"
-	"text/template"
+	"fmt"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -19,7 +17,7 @@ var showCmd = &cobra.Command{
 The show command parses and visualises a file containing erpel ignore rules.
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return ShowRules(os.Stdout, args)
+		return ShowRules(args)
 	},
 }
 
@@ -32,45 +30,14 @@ func init() {
 	showCmd.Flags().BoolVarP(&displayTemplates, "templates", "t", false, "show templates instead of field names")
 }
 
-const outputTemplate = `Rules from {{.Filename}}:
-
-Templates:
-{{range .Templates}}
-{{- .}}
-{{end -}}
-
-`
-
 var (
-	printText  = color.New(color.FgWhite).SprintfFunc()
-	printField = color.New(color.FgGreen).SprintfFunc()
+	printText        = color.New(color.FgWhite).PrintfFunc()
+	printField       = color.New(color.FgHiRed).PrintfFunc()
+	printGlobalField = color.New(color.FgHiBlue).PrintfFunc()
 )
 
-func colorPrinter(templates []erpel.RuleView) []string {
-	list := make([]string, 0, len(templates))
-	for _, template := range templates {
-		var s string
-		for _, field := range template {
-			switch f := field.(type) {
-			case erpel.Text:
-				s += printText("%s", f)
-			case erpel.FieldView:
-				if displayTemplates {
-					s += printField("%s", f.S)
-				} else {
-					s += printField("%s", f.F.Name)
-				}
-			}
-		}
-		list = append(list, s)
-	}
-	return list
-}
-
-var tmpl = template.Must(template.New("output").Parse(outputTemplate))
-
 // ShowRules visualises an erpel rule file.
-func ShowRules(wr io.Writer, args []string) error {
+func ShowRules(args []string) error {
 	if len(args) == 0 {
 		return errors.New("no rule file specified, nothing to do")
 	}
@@ -90,15 +57,27 @@ func ShowRules(wr io.Writer, args []string) error {
 		return err
 	}
 
-	data := struct {
-		Filename  string
-		Templates []string
-	}{
-		filename,
-		colorPrinter(rules.Views()),
-	}
+	fmt.Printf("Rules from %v:\n", filename)
+	for _, rv := range rules.Views() {
+		for _, field := range rv {
+			switch f := field.(type) {
+			case erpel.Text:
+				fmt.Printf("%s", f)
+			case erpel.FieldView:
+				p := printField
+				if f.Global {
+					p = printGlobalField
+				}
 
-	tmpl.Execute(wr, data)
+				if displayTemplates {
+					p("%s", f.S)
+				} else {
+					p("%s", f.F.Name)
+				}
+			}
+		}
+		fmt.Println()
+	}
 
 	return nil
 }
